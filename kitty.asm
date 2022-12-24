11dd87bc93916875842f30a8aabac648344b3cdc
Iprogramprograms <121345725+Iprogramprograms@users.noreply.github.com>
2022-12-24 18:38:19 +0500

    Add files via upload

---
+++ kitty.asm
@@ -0,0 +1,17 @@
+org 7c00h
+start:
+mov ah, 00h
+mov al, 13h
+mov ax, 13h
+int 10h
+mov cx, 64000
+putpixel:
+mov ax, 0A000h
+mov es, ax
+mov ax, cx
+mov di, ax
+mov dl, 0x35
+mov [es:di], dl
+loop putpixel
+times 510-($-7c00h) db 0
+db 055h, 0AAh
