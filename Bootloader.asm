ca04cf413fc0490e337f9de51ae51dba8827162e
Iprogramprograms <121345725+Iprogramprograms@users.noreply.github.com>
2022-12-24 19:58:12 +0500

    Update Bootloader.asm

--- Bootloader.asm
+++ Bootloader.asm
@@ -13,5 +13,14 @@ mov di, ax
 mov dl, 0x35
 mov [es:di], dl
 loop putpixel
+mov cx, 3200
+putpixel2:
+mov ax, 0A000h
+mov es, ax
+mov ax, cx
+mov di, ax
+mov dl, 0x0f
+mov [es:di], dl
+loop putpixel2
 times 510-($-7c00h) db 0
 db 055h, 0AAh
