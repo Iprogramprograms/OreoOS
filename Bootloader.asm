9127e9aba29f511fa2401a61822c44ec660d2592
Iprogramprograms <121345725+Iprogramprograms@users.noreply.github.com>
2023-03-02 21:21:42 +0500

    Update Bootloader.asm

--- Bootloader.asm
+++ Bootloader.asm
@@ -1,12 +1,12 @@
-org 7c00h
+org 7c00h ; Bootloader is at memory adress 7c00h
 mov ax, 13h
-int 10h
+int 10h ; Setting video mode 13h
 xor ax, ax
 mov ds, ax
 mov ss, ax
-mov sp, 0x8000
-jmp switch_to_pm
-GDT_start:
+mov sp, 0x8000 ; idk
+jmp switch_to_pm ; sitching to Protected Mode
+GDT_start: ;here starts GDT (Global Descriptor Table)
 GDT_null:
 dd 0x0
 dd 0x0
@@ -31,17 +31,17 @@ dd GDT_start
 CODE_SEG equ GDT_code - GDT_start
 DATA_SEG equ GDT_data - GDT_start
 switch_to_pm:
-cli
-lgdt [GDT_descriptor]
+cli ; disabling interrupts
+lgdt [GDT_descriptor] ; loading GDT
 mov eax, cr0
 or  eax, 1  ; Set PE=1
 mov cr0, eax
-mov ax, DATA_SEG
+mov ax, DATA_SEG ; initializing protected mode
 mov ds, ax
 jmp CODE_SEG:init_pm
-use32
+use32 ;here we start writing 32-bit code :D
 init_pm:
-mov ebx, 0xA0000000
+mov ebx, 0xA0000000 ; failed attempt to draw on the screem
 mov esi, 50
 mov edi, 100
 mov eax, esi       ; eax = y
@@ -49,7 +49,7 @@ imul eax, 320      ; eax = y * 320
 add eax, edi       ; eax = y * 320 + x
 add ebx, eax       ; ebx = 0xA0000000 + y * 320 + x
 mov byte [ebx], 7  ; set pixel color to white
-hlt
+hlt ; halting the system
 jmp $-1
-times 510-($-$$) db 0
-dw 0xAA55
+times 510-($-$$) db 0 ; filling the gap with 0
+dw 0xAA55 ; Bootloader magic number
